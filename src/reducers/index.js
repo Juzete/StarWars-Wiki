3a164afd019d1ee828ebe4a889f119b4bfaa83bb
Maxim Korzenok <maximka00007777@gmail.com>
2021-08-26 12:09:47 +0300

    initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import wikiReducer from "./wikiReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+  wiki: wikiReducer,
+});
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
---
+++ src/reducers/wikiReducer.js
@@ -0,0 +1,15 @@
+
+
+const defaultState = {
+    items: [],
+    isFetching: true,
+}
+
+export default function wikiReducer(state = defaultState, action) {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
