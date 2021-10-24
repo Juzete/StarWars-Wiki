c721d4cea9e8ffee955a650e08c4c71558ff4705
Maxim Korzenok <maximka00007777@gmail.com>
2021-10-24 20:35:04 +0300

    add avatars in profile

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,11 +8,13 @@ import styles from "./Dashboard.module.css";
 import { Canvas } from "@react-three/fiber";
 import StarsModel from "./Models/Stars/StarsModel";
 import Logo from "./Logo/Logo";
+import { useSelector } from "react-redux";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
   const { currentUser, logout } = useAuth();
   const history = useHistory();
+  const information = useSelector((state) => state.wiki);
 
   async function handleLogout() {
     setError("");
@@ -29,11 +31,17 @@ export default function Dashboard() {
       <Logo />
       <div className={styles.background}>
         <div className={styles.wrapper}>
-          <div>
+          <div className={styles.head}>
             <h2 className="text-center mb-4">Profile</h2>
             {error && <Alert variant="danger">{error}</Alert>}
           </div>
-          <div>
+          <div className={styles.avatar}>
+            <img
+              src={`https://avatars.dicebear.com/api/bottts/${information.currentUser.email}.svg`}
+              alt="avatar"
+            />
+          </div>
+          <div className={styles.email}>
             <strong>Email:</strong> {currentUser.email}
           </div>
           <div className="w-100 text-center mt-2">
